2757bfe2114a039bece1f8f2009a5fdc7b71ceb6
Sparths <70455072+Sparths@users.noreply.github.com>
2026-04-20 09:55:55 +0000

    Add main Models

---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, ForeignKey
+from datetime import datetime
+from .database import Base
+
+class Tracker(Base):
+    __tablename__ = "trackers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    type = Column(String) 
+    start_date = Column(DateTime, default=datetime.utcnow)
+    money_saved_amount = Column(Float, default=0.0)
+    money_saved_per = Column(String)
+    unit = Column(String) 
+    units_per_amount = Column(Float, default=0.0)
+    units_per = Column(String)
+    
+
+    is_active = Column(Boolean, default=True)
+
+
+class JournalEntry(Base):
+    __tablename__ = "journal_entries"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    tracker_id = Column(Integer, ForeignKey("trackers.id"))
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    mood = Column(Integer, nullable=True)
+    content = Column(String)
