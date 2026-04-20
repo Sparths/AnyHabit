5844403accd94359930d0b59b6dad086b566e002
Sparths <70455072+Sparths@users.noreply.github.com>
2026-04-20 15:33:55 +0000

    Change Models for Universal tracking

--- backend/models.py
+++ backend/models.py
@@ -7,15 +7,13 @@ class Tracker(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    type = Column(String) 
+    type = Column(String)
     start_date = Column(DateTime, default=datetime.utcnow)
     money_saved_amount = Column(Float, default=0.0)
     money_saved_per = Column(String)
     unit = Column(String) 
     units_per_amount = Column(Float, default=0.0)
     units_per = Column(String)
-    
-
     is_active = Column(Boolean, default=True)
 
 
@@ -23,8 +21,17 @@ class JournalEntry(Base):
     __tablename__ = "journal_entries"
 
     id = Column(Integer, primary_key=True, index=True)
-
-    tracker_id = Column(Integer, ForeignKey("trackers.id"))
+    tracker_id = Column(Integer, ForeignKey("trackers.id", ondelete="CASCADE"))
     timestamp = Column(DateTime, default=datetime.utcnow)
     mood = Column(Integer, nullable=True)
     content = Column(String)
+
+
+
+class HabitLog(Base):
+    __tablename__ = "habit_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tracker_id = Column(Integer, ForeignKey("trackers.id", ondelete="CASCADE"))
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    amount = Column(Float, default=1.0)
